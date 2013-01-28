7606bbe55332d06f774fad467caafc539d6d59ef
Rodrigo Saito <rodrigo.saito@gmail.com>
2013-01-28 23:28:06 +0400

    Array support

--- lib/roar-sinatra.rb
+++ lib/roar-sinatra.rb
@@ -8,14 +8,20 @@ module Roar
     def roar(object, options = {})
       content_type 'application/hal+json'
 
-      serialize_object object, options
+      if object.is_a? Array
+        object.map do |obj|
+          serialize_object(obj, options)
+        end.to_json
+      else
+        serialize_object(object, options).to_json
+      end
     end
 
     private
 
       def serialize_object(object, options = {})
         representer_class = options[:representer_class] || Object::const_get("#{object.class.name}Representer")
-        object.extend(representer_class).to_json
+        object.extend(representer_class).to_hash
       end
 
   end
--- lib/roar-sinatra/version.rb
+++ lib/roar-sinatra/version.rb
@@ -1,5 +1,5 @@
 module Roar
   module Sinatra
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
